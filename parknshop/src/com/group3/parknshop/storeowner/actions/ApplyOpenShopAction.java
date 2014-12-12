314a82956727073af5aae53548a444074ecae178
QiYunhu <357628953@qq.com>
2014-12-11 21:23:29 -0500

    testChange_AGAIN

--- parknshop/src/com/group3/parknshop/storeowner/actions/ApplyOpenShopAction.java
+++ parknshop/src/com/group3/parknshop/storeowner/actions/ApplyOpenShopAction.java
@@ -22,4 +22,8 @@ public class ApplyOpenShopAction extends ActionSupport {
 	@Resource( name = "applyOpenShopService" )
 	private IApplyOpenShopService applyOpenShopService ;
 
+	private void useless( )
+	{
+		String msg = "just for git to submit and push" ;
+	}
 }
